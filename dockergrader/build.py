42365f1023db0d5919618130efea8e3be8e866f7
Nikita Borisov <git@nikita.ca>
2016-10-01 21:24:12 -0500

    better error reporting

--- dockergrader/build.py
+++ dockergrader/build.py
@@ -4,21 +4,34 @@ from tempfile import NamedTemporaryFile, TemporaryFile
 from pathlib import Path
 import re
 import logging
+import json
 
 log = logging.getLogger(__name__)
 
 BUILD_SUCCESSFUL = re.compile(r'Successfully built ([0-9a-f]+)')
 
+
+class BuildError(Exception):
+    def __init__(self, error, output):
+        self.error = error
+        self.output = output
+
 def _parse_build_output(stream):
-    output = list(stream)
-    m = re.search(BUILD_SUCCESSFUL, output[-1].decode())
+    decstream = list(json.loads(l) for s in stream for l in
+                     s.decode().split('\n') if l)   # capture output
+    output = [x["stream"] for x in decstream if "stream" in x]
+    m = re.search(BUILD_SUCCESSFUL, output[-1])
     if m:
         image = m.group(1)
         log.debug("Built image %s, output %s", m, output)
         return image
     else:
-        log.error("Could not build image: %s", output)
-        raise RuntimeError("Could not build image: {}".format(output))
+        error = [x for x in decstream if "error" in x]
+        if len(error) == 1:
+            error = error[0]
+        log.error("Could not build image: %s; %s", error, output)
+        raise BuildError(error, output)
+
 
 def build_custom_image(dockerfile_str, path, tag=None):
     """ Specifies a dockerfile as a string, and a path to create a custom context """
@@ -34,9 +47,10 @@ def build_custom_image(dockerfile_str, path, tag=None):
         tar.close()
         context_file.seek(0)
         stream = config.docker.build(fileobj=context_file, custom_context=True,
-                                          rm=True, tag=tag)
+                                     rm=True, tag=tag)
 
         return _parse_build_output(stream)
+
 
 def build_image(path, tag=None):
     """ Builds an image for a path. This is just a simple wrapper around
